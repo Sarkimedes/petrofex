8da300b4ebbe6e020ea65c4f3db60156aee4a475
Peter Roberts <peter@blackmarble.com>
2016-03-21 19:11:16 +0000

    added missed files.

---
+++ PetrofexSystem.Pumps.UnitTests/PumpTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PumpLibrary;
+
+namespace PetrofexSystem.Pumps.UnitTests
+{
+    [TestClass]
+    public class PumpTests
+    {
+        [TestMethod]
+        public void Pump_CustomerWaiting_SendsActivateRequestToServer()
+        {
+            var customerGenerator = new FakeCustomerGenerator();
+            var server = new FakeServer();
+            var pump = new Pump(server, customerGenerator);
+            
+            customerGenerator.InvokeCustomerReady(new CustomerReadyEventArgs(FuelType.Diesel));
+
+            Assert.IsTrue(server.ActivationRequested);
+        }
+
+        [TestMethod]
+        public void Pump_CustomerReady_SetsNextTransactionToSuppliedFuelType()
+        {
+            var customerGenerator = new FakeCustomerGenerator();
+            var server = new FakeServer();
+            var pump = new Pump(server, customerGenerator);
+
+            customerGenerator.InvokeCustomerReady(new CustomerReadyEventArgs(FuelType.Diesel));
+
+            Assert.AreEqual(pump.NextTransaction.FuelType, FuelType.Diesel);
+        }
+    }
+}
