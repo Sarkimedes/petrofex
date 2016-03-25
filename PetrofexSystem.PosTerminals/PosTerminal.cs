96299886c9583c6a3e10975cc83744f242c8fa9c
Sarkimedes <peter.w.roberts93@gmail.com>
2016-03-25 20:25:54 +0000

    Made list of active pumps static

--- PetrofexSystem.PosTerminals/PosTerminal.cs
+++ PetrofexSystem.PosTerminals/PosTerminal.cs
@@ -7,15 +7,28 @@ namespace PetrofexSystem.PosTerminals
 {
     public class PosTerminal
     {
+        // TODO: Hack. This should probably be taken from a central service.
+        private static readonly Dictionary<Guid, bool> PumpActivationsById = new Dictionary<Guid, bool>();
 
-        public void ReceiveActivationRequest(Guid pumpId)
+        public PosTerminal()
         {
-            
+        }
+
+        public void ActivatePump(Guid pumpId)
+        {
+            if (PumpActivationsById.ContainsKey(pumpId))
+            {
+                PumpActivationsById[pumpId] = true;
+            }
+            else
+            {
+                PumpActivationsById.Add(pumpId, true);
+            }
         }
 
         public bool PumpIsActive(Guid pumpId)
         {
-            return true;
+            return PumpActivationsById.ContainsKey(pumpId) && PumpActivationsById[pumpId];
         }
     }
 }
