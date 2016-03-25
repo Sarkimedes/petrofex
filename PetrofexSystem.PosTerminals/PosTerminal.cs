49231267d42f26dcd31de1a38a8ef6f363394d08
Sarkimedes <peter.w.roberts93@gmail.com>
2016-03-25 19:45:42 +0000

    Added ReceiveActivationRequest method

---
+++ PetrofexSystem.PosTerminals/PosTerminal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetrofexSystem.PosTerminals
+{
+    public class PosTerminal
+    {
+
+        public void ReceiveActivationRequest(Guid pumpId)
+        {
+            
+        }
+
+        public bool PumpIsActive(Guid pumpId)
+        {
+            return true;
+        }
+    }
+}
