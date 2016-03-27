89c2af1c717e33d557913645364929cb7397fbb4
Sarkimedes <peter.w.roberts93@gmail.com>
2016-03-27 21:24:35 +0100

    Finished implementing pump state manager

--- PetrofexSystem.PosTerminals.UnitTests/PumpStateManagerTests.cs
+++ PetrofexSystem.PosTerminals.UnitTests/PumpStateManagerTests.cs
@@ -14,9 +14,40 @@ namespace PetrofexSystem.PosTerminals.UnitTests
     public class PumpStateManagerTests
     {
         [TestMethod]
-        public void SetState_FromInactiveToCustomerWaiting_ShouldUpdatePumpState()
+        public void Constructor_ShouldSetStateToInactive()
         {
-            
+            var stateManager = new PumpStateManager();
+
+            Assert.AreEqual(PumpState.Inactive, stateManager.CurrentState);
+        }
+
+        [TestMethod]
+        public void SetState_FromInactiveToCustomerWaiting_ShouldSetStateToCustomerWaiting()
+        {
+            var stateManager = new PumpStateManager();
+
+            stateManager.SetState(PumpState.CustomerWaiting);
+
+            Assert.AreEqual(PumpState.CustomerWaiting, stateManager.CurrentState);
+        }
+
+        [TestMethod]
+        public void SetState_FromInactiveToInactive_ShouldSetStateToInactive()
+        {
+            var stateManager = new PumpStateManager();
+
+            stateManager.SetState(PumpState.Inactive);
+
+            Assert.AreEqual(PumpState.Inactive, stateManager.CurrentState);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void SetState_FromInactiveToActivationPending_ShouldThrowError()
+        {
+            var stateManager = new PumpStateManager();
+
+            stateManager.SetState(PumpState.ActivationPending);
         }
     }
 }
