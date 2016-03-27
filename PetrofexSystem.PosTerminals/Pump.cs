790fd14350a6738fd70f385b8ce1094eb28a128c
Sarkimedes <peter.w.roberts93@gmail.com>
2016-03-27 21:56:40 +0100

    Added factory for pumps. Added tests for active pumps

---
+++ PetrofexSystem.PosTerminals/Pump.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetrofexSystem.PosTerminals
+{
+    /// <summary>
+    /// Models a pump as POS terminals see it.
+    /// </summary>
+    public class Pump
+    {
+        private readonly PumpStateManager _stateManager;
+
+        public Pump(string id)
+        {
+            this.Id = id;
+
+            this._stateManager = new PumpStateManager();
+            this._stateManager.SetState(PumpState.CustomerWaiting);
+        }
+
+        public string Id { get; private set; }
+
+        public PumpState CurrentState
+        {
+            get
+            {
+                return this._stateManager.CurrentState;
+            }
+        }
+
+        public void Activate()
+        {
+            this._stateManager.SetState(PumpState.ActivationPending);
+        }
+    }
+}
---
+++ PetrofexSystem.PosTerminals/PumpFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetrofexSystem.PosTerminals
+{
+    public class PumpFactory
+    {
+        private readonly ICollection<Pump> _pumps;
+
+        public PumpFactory()
+        {
+            this._pumps = new List<Pump>();
+        }
+
+        public Pump HandleActivationRequest(string pumpId)
+        {
+            var pump = this._pumps.FirstOrDefault(x => x.Id == pumpId);
+            if (pump != null)
+            {
+                return pump;
+            }
+
+            var addedPump = new Pump(pumpId);
+            this._pumps.Add(addedPump);
+            return addedPump;
+        }
+    }
+}
