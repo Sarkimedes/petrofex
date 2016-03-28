1f90521fa1a95b6a773d6e30b31b52ea408c60a8
Sarkimedes <peter.w.roberts93@gmail.com>
2016-03-28 22:35:18 +0100

    Added payment server interface.

---
+++ PetrofexSystem.PosTerminals/IPaymentServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PetrofexSystem.Common;
+
+namespace PetrofexSystem.PosTerminals
+{
+    public interface IPaymentServer
+    {
+        void SendForProcessing(Transaction transaction);
+    }
+}
