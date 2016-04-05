330e63e48fee81f8b7b6c8693aa66f7826b83dbd
Peter Roberts <peter@blackmarble.com>
2016-04-05 12:00:08 +0100

    Added pump activation server class

---
+++ PetrofexSystem.Server/INotifyPumpActivated.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetrofexSystem.Server
+{
+    public interface INotifyPumpActivated
+    {
+        event Action<string> PumpActivationRequested;
+        event Action<string> PumpDeactivationRequested;
+    }
+}
---
+++ PetrofexSystem.Server/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace PetrofexSystem.Server
+{
+    public class Server : IPumpActivationServer, INotifyPumpActivated
+    {
+        public void RequestActivation(string pumpId)
+        {
+            if (PumpActivationRequested != null)
+            {
+                this.PumpActivationRequested(pumpId);
+            }
+        }
+
+        public void RequestDeactivation(string pumpId)
+        {
+            if (PumpDeactivationRequested != null)
+            {
+                this.PumpDeactivationRequested(pumpId);
+            }
+        }
+
+        public event Action<string> PumpActivationRequested;
+        public event Action<string> PumpDeactivationRequested;
+    }
+}
